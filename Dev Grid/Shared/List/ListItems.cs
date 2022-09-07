fcbdbfca719d541c6a7849772d87a09e0f44475f
Afraz <Afrazniazi15@gmail.com>
2022-09-07 23:29:03 +0500

    Dev Grid Issue

---
+++ Dev Grid/Shared/List/ListItems.cs
@@ -0,0 +1,15 @@
+﻿namespace Dev_Grid.Shared.List
+{
+    public class ListItems
+    {
+       
+            public string? FirstName { get; set; }
+            public string? LastName { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? Birth { get; set; }
+            public string? Hire { get; set; }
+
+
+    }
+}
