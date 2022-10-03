3437014671831c0fa893d6301c359a409d17e21f
Afraz <Afrazniazi15@gmail.com>
2022-10-03 14:14:58 +0500

    Few Changes

---
+++ CRUD Web App/Controllers/DepartmentController.cs
@@ -0,0 +1,44 @@
+﻿using CRUD_Web_App.Data;
+using CRUD_Web_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Web_App.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public DepartmentController(MVCDbContext DeptMVCcontext)
+        {
+            this.DeptMVCcontext = DeptMVCcontext;
+        }
+
+        public MVCDbContext DeptMVCcontext { get; }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CodeError()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddDepartment addDepartmentRequest)
+        {
+            var department = new Departments()
+            {
+                DepartmentName = addDepartmentRequest.DepartmentName,
+            };
+
+            if (addDepartmentRequest.DepartmentAdmin == "111")
+            {
+                DeptMVCcontext.Departments.Add(department);
+                DeptMVCcontext.SaveChanges();
+                return RedirectToAction("Add");
+            }
+            return RedirectToAction("Error");
+        }
+    }
+}
